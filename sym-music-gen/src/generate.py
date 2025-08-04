85ba8fa04ad3a924719869808dc079e90f7166a8
John Backsund <john.backsund@ingka.com>
2025-08-04 21:41:40 +0200

    Added generation script that dumps midi file.

--- sym-music-gen/src/generate.py
+++ sym-music-gen/src/generate.py
@@ -1,4 +1,6 @@
 import argparse
+import torch
+import symusic
 import miditok
 import transformers
 import json
@@ -15,19 +17,53 @@ def __parse_args():
     return parser.parse_args()
 
 
+def notes_to_score(notes):
+    score = symusic.Score(ttype=symusic.TimeUnit.quarter)
+
+    track = symusic.Track(ttype=symusic.TimeUnit.quarter)
+
+    for n in notes:
+        note = symusic.Note(
+            time=n["start"] / 4,
+            duration=n["duration"]
+            / 4,  # The note duration is given as fraction of a quarter
+            pitch=n["pitch"],
+            velocity=90,
+            ttype=symusic.TimeUnit.quarter,
+        )
+        track.notes.append(note)
+
+    score.tracks.append(track)
+
+    score = score.to("tick")
+
+    return score
+
+
 def main():
     args = __parse_args()
 
-    model = transformers.AutoModelForCausalLM.from_pretrained(args.model).eval().cuda()
+    model = transformers.AutoModelForCausalLM.from_pretrained(args.model).eval()
 
     tokenizer = miditok.REMI.from_pretrained(args.model)
 
     with args.prompt.open() as io:
         prompt = json.load(io)
 
-    import pdb
+    prompt_score = notes_to_score(prompt["prompt"])
 
-    pdb.set_trace()
+    prompt_tokens = tokenizer.encode(prompt_score)[0].ids
+
+    prompt_tokens_pt = torch.tensor(prompt_tokens).unsqueeze(0)
+    attention_mask = torch.ones_like(prompt_tokens_pt)
+
+    with torch.no_grad():
+        output = model.generate(
+            prompt_tokens_pt, attention_mask=attention_mask, max_length=200
+        )
+    full_score = tokenizer.decode(output)
+
+    full_score.dump_midi(f"{args.prompt.stem}_generation.mid")
 
 
 if __name__ == "__main__":
