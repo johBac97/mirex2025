dafef5f67f0ea9938880b8bb39820f490d0cde7b
John Backsund <john.backsund@ingka.com>
2025-08-04 20:36:44 +0200

    work.

--- sym-music-gen/src/generate.py
+++ sym-music-gen/src/generate.py
@@ -1,5 +1,7 @@
 import argparse
+import miditok
 import transformers
+import json
 
 from pathlib import Path
 
@@ -8,6 +10,7 @@ def __parse_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("model", type=Path)
+    parser.add_argument("prompt", type=Path)
 
     return parser.parse_args()
 
@@ -16,6 +19,15 @@ def main():
     args = __parse_args()
 
     model = transformers.AutoModelForCausalLM.from_pretrained(args.model).eval().cuda()
+
+    tokenizer = miditok.REMI.from_pretrained(args.model)
+
+    with args.prompt.open() as io:
+        prompt = json.load(io)
+
+    import pdb
+
+    pdb.set_trace()
 
 
 if __name__ == "__main__":
